f93e1c617ecd5f1572334f8e394f1a9f21ac14bc
Jordan Pitlor <jordan@pitlor.dev>
2021-07-02 21:03:27 -0400

    initial commit

---
+++ src/test/kotlin/dev/pitlor/scattergories/ScattergoriesApplicationTests.kt
@@ -0,0 +1,13 @@
+package dev.pitlor.scattergories
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ScattergoriesApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
