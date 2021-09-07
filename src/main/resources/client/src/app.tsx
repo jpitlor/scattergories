65112dfaa1baec115d7ca3f79731c603c2bed5b4
Jordan Pitlor <jordan@pitlor.dev>
2021-09-07 07:47:57 -0400

    start on join game design

--- src/main/resources/client/src/app.tsx
+++ src/main/resources/client/src/app.tsx
@@ -6,9 +6,9 @@ import Logo from "./components/Logo";
 export default function App() {
   return (
     <div className="w-full h-full absolute bg-neutral-20">
-      {true && <Logo className="bg-red-300 pb-36" />}
+      <Logo className="bg-red-300 pb-36" />
       <div className="container -mt-32 my-8 py-8 mx-auto rounded-md shadow-md bg-neutral-0">
-        {true && <CreateProfile />}
+        {false && <CreateProfile />}
         {true && <JoinGame />}
         {true && <PlayGame />}
       </div>
--- src/main/resources/client/src/pages/JoinGame.tsx
+++ src/main/resources/client/src/pages/JoinGame.tsx
@@ -1,3 +1,32 @@
+import Form, { Field, FormFooter } from "@atlaskit/form";
+import Button, { ButtonGroup } from "@atlaskit/button";
+import TextField from "@atlaskit/textfield";
+
 export default function JoinGame() {
-  return <div />;
+  function onSubmit() {}
+
+  return (
+    <Form onSubmit={onSubmit}>
+      {({ formProps }) => (
+        <form {...formProps} className="prose mx-auto">
+          <h1 className="text-center">Join a Game</h1>
+          <Field
+            name="shouldCreateGame"
+            label="Select Choice"
+            defaultValue=""
+            isRequired
+          >
+            {({ fieldProps }) => <TextField {...fieldProps} />}
+          </Field>
+          <FormFooter>
+            <ButtonGroup>
+              <Button type="submit" appearance="primary" className="mt-8">
+                Join Game
+              </Button>
+            </ButtonGroup>
+          </FormFooter>
+        </form>
+      )}
+    </Form>
+  );
 }
